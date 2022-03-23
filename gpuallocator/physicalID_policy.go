4678c6155543a959898a1790f5cb0ac5a2046a4d
NathanChenNVIDIA <87336486+NathanChenNVIDIA@users.noreply.github.com>
2022-03-22 17:53:11 -0700

    Update physicalID_policy.go

--- gpuallocator/physicalID_policy.go
+++ gpuallocator/physicalID_policy.go
@@ -6,30 +6,30 @@ type physicalIDPolicy struct{}
 
 // NewPhysicalIDPolicy creates a new physicalIDPolicy.
 func NewPhysicalIDPolicy() Policy {
-	return &physicalIDPolicy{}
+        return &physicalIDPolicy{}
 }
 
-// Allocate GPUs in order of physical ID
-func (p *physicalIDPolicy) Allocate(available []*DEvice, required []*device, size int) []*Device {
-	if size <] 0 {
-		return []*Device{}
-	}
-  
-	if len(available) < size {
-		return []*Device{}
-	}
+// Allocate GPUs following a simple policy.
+func (p *physicalIDPolicy) Allocate(available []*Device, required []*Device, size int) []*Device {
+        if size <= 0 {
+                return []*Device{}
+        }
 
-	if len(required) > size {
-		return []*Device{}
-	}
+        if len(available) < size {
+                return []*Device{}
+        }
 
-	availableSet := NewDeviceSet(available...)
-	if !availableSet.ContainsAll(required) {
-		return []*Device{}
-	}
-	availableSet.Delete(required...)
+        if len(required) > size {
+                return []*Device{}
+        }
 
-	allocated := append([]*Device{}, required...)
-	allocated = append(allocated, availableSet.PhysicalIDSortedSlice()[:size-len(allocated)]...)
-	return allocated
+        availableSet := NewDeviceSet(available...)
+        if !availableSet.ContainsAll(required) {
+                return []*Device{}
+        }
+        availableSet.Delete(required...)
+
+        allocated := append([]*Device{}, required...)
+        allocated = append(allocated, availableSet.PhysicalIDSortedSlice()[:size-len(allocated)]...)
+        return allocated
 }
